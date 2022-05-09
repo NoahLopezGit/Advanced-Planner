08eff1a2dc09046c63d888e9722674502f027c1c
Kyle-Ortiz <kyleaortiz@gmail.com>
2022-05-08 20:16:23 -0500

    get rekt

--- My_Planner_Site/polls/models.py
+++ My_Planner_Site/polls/models.py
@@ -1,12 +1,26 @@
 from secrets import choice
 from django.db import models
+from django.utils import timezone
+import datetime
+
 
 # Create your models here.
 class Question(models.Model):
+
+     def __str__(self):
+           return self.question_text
+
+     def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
      question_text = models.CharField(max_length=200)
      pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
+
+     def __str__(self):
+               return self.choice_text
+
      question = models.ForeignKey(Question, on_delete=models.CASCADE)
      choice_text = models.CharField(max_length=200)
      vote = models.IntegerField(default=0)
