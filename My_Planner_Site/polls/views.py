79446e1f0449487e181219ad57785e439b3a3bbe
Noah Lopez <loalno12@gmail.com>
2022-05-24 19:59:55 -0700

    not sure what this is

--- My_Planner_Site/polls/views.py
+++ My_Planner_Site/polls/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-
+from .models import Question, Choice
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<br> '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+# Leave the rest of the views (detail, results, vote) unchanged
