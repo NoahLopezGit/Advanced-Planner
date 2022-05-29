de65388f6023c8b81d993be5f2e8ed68165cceac
Noah Lopez <loalno12@gmail.com>
2022-05-29 10:43:02 -0700

    adding code for generic views

--- My_Planner_Site/polls/urls.py
+++ My_Planner_Site/polls/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name='polls' #tells {% url 'polls:name' %} that polls points to this app, "Namespacing url names"
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('myview/',views.myview, name='myview') #what is the name doing
+    path('', views.IndexView.as_view(), name="index"), #this is inserting the view defined under views.IndexView.as_view() instead of what we have defined under our views.py
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'), #seems we are keeping our vote view
 ]
--- My_Planner_Site/polls/views.py
+++ My_Planner_Site/polls/views.py
@@ -1,50 +1,35 @@
-
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from .models import Question, Choice
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-# Create your views here.
+from django.views import generic
+
+from .models import Choice, Question
+
+
+#notice default views are generated with classes, wheras our custom views are defined under functions
+class IndexView(generic.ListView): #IndexView is inheriting attributes of generic.ListView
+    template_name = 'polls/index.html' #does this not have a default template it looks for ?
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView): 
+    #if this i not specified it will look for template under name of <app name>/<model name>_detail.html; so polls/question_detail.html
+    #presumably under the templates directory in polls
+    model = Question 
+    #template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request,'polls/index.html', context) #why does this method need the request object?
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id) #pk is primary key, question unique identifier
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) #it seems this has more complex handing than just positional logic
-
-
-def myview(request): #for some reason you must pass request to this function
-    return HttpResponse("myview")
-
-
-# Leave the rest of the views (detail, results, vote) unchanged
+    ... # same as above, no changes needed.
